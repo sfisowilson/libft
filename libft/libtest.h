1cd62f757eeda7df4d5d377da5171e911c67891a
sfisowilson <loverboy.sfiso@gmail.com>
2018-05-30 20:48:03 +0200

    makefile done

---
+++ libft/libtest.h
@@ -0,0 +1,8 @@
+#ifndef LIBTEST_H
+# define LIBTEST_H
+	#define	STRING_1	"the most hidden seret is ...\0I glow in the dark\n\n\r"
+	#define	STRING_2	"What if I ignored you?"
+	#define	STRING_3	"why do I need to test everything myself?"
+	#define	STRING_4	"maybe I can lol !"
+
+#endif
